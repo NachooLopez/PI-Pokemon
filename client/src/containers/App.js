c04dd617f185f57168fb82ab2db2604d347260bd
NachooLopez <ignacio.fco.lopez@gmail.com>
2022-03-11 15:47:53 -0300

    fixes landing

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/landing" element={<Landing />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/home" element={<Home />} />
         <Route path="/pokemons/:id" element={<Details />} />
         <Route path="/create" element={<Create />} />

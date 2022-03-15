b26ab510ef00b80532870bb86e07d4ba6e1716ae
NachooLopez <ignacio.fco.lopez@gmail.com>
2022-03-15 18:52:12 -0300

    deploy

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-// import styles from "./Landing.module.css";
+import styles from "./Landing.module.css";
 import logo from "../../img/Pokelogo.png";
 import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
     <div>
-      <div />
-      <div>
-        <img src={logo} alt="Logo" />
+      <div className={styles.background} />
+      <div className={styles.container}>
+        <img className={styles.logo} src={logo} alt="Logo" />
         <Link to="/home">
-          <button>Entrar</button>
+          <button className={styles.button}>Entrar</button>
         </Link>
       </div>
     </div>
